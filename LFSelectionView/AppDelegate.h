98ca25efe7dc66e4e338aed9634be9142e59c569
刘丰 <liufeng@liufengdemac-mini.local>
2015-12-22 16:29:26 +0800

    Initial Commit

---
+++ LFSelectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFSelectionView
+//
+//  Created by LiuFeng on 15/12/22.
+//  Copyright © 2015年 LiuFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFSelectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LFSelectionView
+//
+//  Created by LiuFeng on 15/12/22.
+//  Copyright © 2015年 LiuFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
